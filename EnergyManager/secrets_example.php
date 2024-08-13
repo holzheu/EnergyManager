11bdaa676a0b699fc51d0a1aeaf829fd64af7d6c
Stefan Holzheu <stefan.holzheu@uni-bayreuth.de>
2024-08-13 12:33:23 +0200

    0.0.5b
    + add secrets_example.php

---
+++ EnergyManager/secrets_example.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Constants with sensible information
+ * 
+ */
+
+define("Solarprognose_access_token","SecretAccessToken");
+define("Solarprognose_plant_id",13432);
+define("OpenMeteo_latitude",50.4313);
+define("OpenMeteo_longitude",11.8315);
+define("BEV_DIV_ip","192.168.1.4");
+define("Kostal_Plenticore_Plus_ip",'192.168.1.5');
+define("BayEOS_USER",'import');
+define("BayEOS_PW",'import');
+define("BayEOS_IP",'192.168.1.6');
